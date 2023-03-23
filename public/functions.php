0adbc78f8db3479c00fa586cae3c02a903e5b33d
Mattciao96 <matt.ciao@gmail.com>
2023-03-23 15:03:38 +0100

    tablecreator qua funziona

---
+++ public/functions.php
@@ -0,0 +1,32 @@
+<?php
+
+function array_flatten($array,$return) {
+	for($x = 0; $x <= count($array); $x++) {
+		if(is_array($array[$x])) {
+			$return = array_flatten($array[$x], $return);
+		}
+		else {
+			if(isset($array[$x])) {
+				$return[] = $array[$x];
+			}
+		}
+	}
+	return $return;
+}
+/**
+ * Nel momento in cui 2 valori delle colonne devono essere uniti in uno (guarda json connection.json) gestisce il tutto
+ * 
+ */
+function mergeStringValues($rowContent, $columnsToMerge, $separator) {
+  if ($rowContent[$columnsToMerge[0]] != '' and $rowContent[$columnsToMerge[1]] != '') {
+    $stringToSearch = $rowContent[$columnsToMerge[0]] . $separator . $rowContent[$columnsToMerge[1]];
+  } elseif ($rowContent[$columnsToMerge[0]] != '' and $rowContent[$columnsToMerge[1]] == '') {
+    $stringToSearch = $rowContent[$columnsToMerge[0]];
+  } elseif ($rowContent[$columnsToMerge[0]] == '' and $rowContent[$columnsToMerge[1]] != '') {
+    $stringToSearch = $rowContent[$columnsToMerge[1]];
+  } else {
+    $stringToSearch = '';
+  }
+
+  return $stringToSearch;
+}
--- public/tablecreator.php
+++ public/tablecreator.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 ini_set("memory_limit", "4096M");
 
 require_once('../config/init.php');
-
+include('functions.php');
 
 
 
@@ -13,6 +13,7 @@ require_once('../config/init.php');
 $mysqli = new mysqli($database_adress, $database_login, $database_password, $database_name);
 
 
+# partiamo dalla tabella rilievi
 // 0 parte bonus, converti le date se queste sono un numero grezzo di excel
 $query = "SELECT DISTINCT Data FROM rilievi WHERE Data IS NOT NULL";
 $result = $mysqli->query($query);
@@ -44,7 +45,7 @@ foreach ($arrayResult as $string) {
   }
 }
 
-# partiamo dalla tabella rilievi
+
 // 1: controlla se esiste la colonna rilievi, altrimenti creala e inserisci numeri autoincrementali
 echo 'RILIEVI______________________________________________________________________________________<br><br>';
 
@@ -81,7 +82,140 @@ Habitat originale as habitat,
 Incertezza m
 Quota_dem as
 ,  FROM table_name"; */
+
+
+
+$jsonData = file_get_contents('connection.json');
+$jsonData = json_decode($jsonData, true);
+
+/* echo '<pre>';
+print_r($jsonData);
+echo '<pre>'; */
+
+$columnsData = $jsonData['location'];
+//print_r($columnsData);
+
+
+
 $query = "SELECT * FROM rilievi";
+$result = $mysqli->query($query);
+$arrayResult = [];
+if ($result->num_rows > 0) {
+  while ($row = $result->fetch_assoc()) {
+    array_push($arrayResult, $row);
+  }
+}
+
+/* $columnsInRilievi = array_column($columnsData, 'rilievi');
+//$columnsTest = array_merge($columnsInRilievi);
+$columnsInRilievi = array_flatten($columnsInRilievi, array());
+ */
+
+$arrayResult = $arrayResult[1847];
+$query = 'SELECT id_location FROM location WHERE ';
+
+/* foreach ($columnsInRilievi as $column) {
+  
+} */
+
+// parte di test
+// abbiamo arrayresult la prima riga di rilievi $arrayResult 
+// 
+//$columnsData = $columnsData[0];
+echo '<pre>';
+print_r($columnsData);
+echo '<pre>';
+echo '<pre>';
+print_r($arrayResult);
+echo '<pre>';
+
+// caso in cui non ci sono poblemi
+//
+
+foreach ($columnsData as $column) {
+
+
+  // Questo serve a gestire il caso in cui devo unire 2 valori in un' unica nuova colonna
+  if (is_array($column['rilievi'])) {
+
+    $stringToSearch = mergeStringValues($arrayResult, $column['rilievi'], '; ');
+
+
+    if ($stringToSearch == '') {
+      $query .= "{$mysqli->real_escape_string($column['name'])} IS NULL";
+    } {
+      $query .= "{$mysqli->real_escape_string($column['name'])} LIKE '{$mysqli->real_escape_string($stringToSearch)}'";
+    }
+  } else {
+
+    if (
+      $column['rilievi'] == '' or
+      $arrayResult[$column['rilievi']] == ''
+    ) {
+      $query .= "{$mysqli->real_escape_string($column['name'])} IS NULL";
+    } else {
+      $query .= "{$mysqli->real_escape_string($column['name'])} LIKE '{$mysqli->real_escape_string($arrayResult[$column['rilievi']])}'";
+    }
+  }
+
+  $query.= ' AND ';
+}
+$query = substr($query , 0, -4);
+echo $query;
+
+$result = $mysqli->query($query);
+
+
+
+/* **************************************************************************************************************************************** */
+/* 
+$query = 'INSERT INTO location ';
+$queryInto = '(';
+$queryValues = '(';  
+if ($result->num_rows == 0) {
+
+
+  foreach ($columnsData as $column) {
+
+
+    // Questo serve a gestire il caso in cui devo unire 2 valori in un' unica nuova colonna
+    if (is_array($column['rilievi'])) {
+  
+      $stringToSearch = mergeStringValues($arrayResult, $column['rilievi'], '; ');
+  
+  
+      if ($stringToSearch == '') {
+        $query .= "{$mysqli->real_escape_string($column['name'])} IS NULL";
+        $queryInto = '(';
+        $queryValues = '('; 
+      } {
+        $query .= "{$mysqli->real_escape_string($column['name'])} LIKE '{$mysqli->real_escape_string($stringToSearch)}'";
+      }
+    } else {
+  
+      if (
+        $column['rilievi'] == '' or
+        $arrayResult[$column['rilievi']] == ''
+      ) {
+        $query .= "{$mysqli->real_escape_string($column['name'])} IS NULL";
+      } else {
+        $query .= "{$mysqli->real_escape_string($column['name'])} LIKE '{$mysqli->real_escape_string($arrayResult[$column['rilievi']])}'";
+      }
+    }
+  
+    $query.= ' AND ';
+  }
+  $query = substr($query , 0, -4);
+  echo $query;
+}
+
+ */
+
+
+
+
+
+/* $query = "SELECT * FROM rilievi";
 $result = $mysqli->query($query);
 
 $arrayResult = [];
@@ -92,4 +226,4 @@ if ($result->num_rows > 0) {
 }
 echo '<pre>';
 print_r($arrayResult[0]);
-echo '<pre>';
+echo '<pre>'; */
